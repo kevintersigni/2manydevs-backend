cc1a6448d9a60326226672de8dcdbf5878e6ca1b
Kevin Tersigni <kevintersigni@MacBook-Air-de-Kevin.local>
2021-11-09 10:46:42 +0100

    change WYSIWYG with Strapi CKEditor5 plugin

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e1baf22344b6771c0c507452f8b2de86'),
+      secret: env("ADMIN_JWT_SECRET", "e1baf22344b6771c0c507452f8b2de86"),
     },
   },
 });
